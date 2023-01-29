a3a6ab2c04a535b9c348362753f60b569a8ca6e4
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-01-29 10:22:33 +0600

    structure added

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from '../components/Home/Home';
+import Footer from '../components/Shared/Footer';
+import Header from '../components/Shared/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Home></Home>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h4>This is Home Component.</h4>
+            <h4 className=' text-blue-600'>This is Home Component.</h4>
         </div>
     );
 };
